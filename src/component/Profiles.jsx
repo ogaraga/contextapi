a6f458d49570b94617b637b6d34e8edfc5074bf1
Ogaraga <raymond.ogaraga@gmail.com>
2024-01-31 07:15:27 +0100

    REVIEW

--- src/component/Profiles.jsx
+++ src/component/Profiles.jsx
@@ -1,7 +1,7 @@
 import { useContext} from "react";
 import UserContext from "../context/UserContext";
 import Login from "./Login";
-import Modal from './modal';
+import Modal from './Modal';
 function Profiles() {
   
 
